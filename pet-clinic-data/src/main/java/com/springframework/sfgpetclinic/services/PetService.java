6716295ae588aad66c029c48b514d281ad1d727f
Vildana Suta <vildana.suta@edu.fit.ba>
2022-12-14 15:41:12 +0100

    Implemented map based services

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.springframework.sfgpetclinic.services;
 
 import com.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long>{
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/VetService.java
@@ -2,10 +2,7 @@ package com.springframework.sfgpetclinic.services;
 
 import com.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
+
 }
