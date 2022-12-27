0376dd1986f833248f17d275568dae8300226bbf
Vildana Suta <vildana.suta@edu.fit.ba>
2022-12-27 13:42:14 +0100

    Created vet speciality entity. Closes #24

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package com.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
 
+public class Vet extends Person{
+    Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
